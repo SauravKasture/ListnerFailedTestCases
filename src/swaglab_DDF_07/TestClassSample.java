8cf9fad4d171f331c4401e479ae47cc2a23546a5
daduk <daduk@192.168.1.35>
2023-01-28 16:50:24 +0530

    [Saurav]:Test Sample Class

---
+++ src/swaglab_DDF_07/TestClassSample.java
@@ -0,0 +1,16 @@
+package swaglab_DDF_07;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClassSample 
+{
+	@Test
+	
+	public void m1()
+	{
+		WebDriver m = new ChromeDriver();
+	}
+
+}
